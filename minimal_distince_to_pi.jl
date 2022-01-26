be61cfa2c00791366887e6275f745020795d880b
aiv2008 <aiv2005@sina.com>
2022-01-26 18:14:38 +0800

    modify the logmin function, unfinished

--- minimal_distince_to_pi.jl
+++ minimal_distince_to_pi.jl
@@ -28,6 +28,23 @@ end
 # minimalDistanceToPi(482480,1196809)
 # ---sample end---
 
+
+function logmin(vect :: Vector{Float64}, startindex::Int64, endindex::Int64)
+    # v = vect[startindex:endindex]
+    if isempty(vect) ==true return (-1, nothing)
+    elseif  length(vect[startindex:endindex]) == 1 return (startindex, vect[startindex])
+    elseif length(vect[startindex:endindex]) == 2 return ( (isequal(vect[startindex], min(vect[startindex],v[endindex])) ? vect[startindex] : vect[endindex] ),   min(vect[startindex],vect[endindex]))
+    else 
+        midindex = isequal(length(v) % 2, 0)
+        (leftindex,leftval) = logmin(vect, startindex, startindex+Int(length(v)/2)-1)
+        (rightindex,rightval) = logmin(vect, startindex+Int(length(v)/2), endindex)
+        println("leftval=$leftval, rightval=$rightval")
+        # resu = (isequal(leftval,min(leftval, rightval)) == true) ? (leftindex, leftval):(rightindex, rightval)
+        return (-1,-3)
+    end
+end
+   
+
 function indexof(s::AbstractString, inst::AbstractChar)
     index = 0
     for i = 1 : length(s)
@@ -42,9 +59,12 @@ end
 function __init__()
     s = readline()
     index = indexof(s, ' ')
-    result = minimalDistanceToPi(parse(Int64,s[1:(index-1)]),parse(Int64,  s[(index+1):end]))
-    println("result = $result")
+    # result = minimalDistanceToPi(parse(Int64,s[1:(index-1)]),parse(Int64,  s[(index+1):end]))
+    # println("result = $result")
 end
 
-__init__()
+# __init__()
+
+vvvv = [1.2,2.2,3.4,1.1,1.2,567.645]
+println(logmin(vvvv, 1, lastindex(vvvv)))
 
